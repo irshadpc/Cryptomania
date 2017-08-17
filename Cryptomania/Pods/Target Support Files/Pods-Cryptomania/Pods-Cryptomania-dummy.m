b39161e527cc04456883fcd6241aef4c7799793a
Aleix <aleix@Mac-mini-de-Aleix.local>
2017-08-17 21:11:12 +0200

    Initial setup

---
+++ Cryptomania/Pods/Target Support Files/Pods-Cryptomania/Pods-Cryptomania-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Cryptomania : NSObject
+@end
+@implementation PodsDummy_Pods_Cryptomania
+@end
---
+++ Cryptomania/Pods/Target Support Files/Pods-CryptomaniaUITests/Pods-CryptomaniaUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CryptomaniaUITests : NSObject
+@end
+@implementation PodsDummy_Pods_CryptomaniaUITests
+@end
